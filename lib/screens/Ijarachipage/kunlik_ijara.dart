601d9576e56d318a235c5ca92fd167ee62eb3949
nizomiddiniminjonov11 <nizomiddiniminjonov11@gmail.com>
2022-09-08 18:09:53 +0500

    kunlik kv page api ready

--- lib/screens/Ijarachipage/kunlik_ijara.dart
+++ lib/screens/Ijarachipage/kunlik_ijara.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:talaba_uy/core/const/app_colors.dart';
+import 'package:talaba_uy/models/days_apartment.dart';
 import 'package:talaba_uy/screens/All_Ads_Page/detail_page.dart';
 import 'package:talaba_uy/screens/Ijarachipage/filtr.dart';
+import 'package:talaba_uy/services/days_service.dart';
 
 class KunlikIjara extends StatefulWidget {
   const KunlikIjara({Key? key}) : super(key: key);
@@ -96,95 +98,107 @@ class _KunlikIjaraState extends State<KunlikIjara> {
         body: TabBarView(
           controller: _tabController,
           children: [
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: 20,
-                itemBuilder: (BuildContext contex, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 324.w,
-                            height: 100.h,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(6.r),
-                                color: AppColors.secondBackgroud),
+            FutureBuilder<List<DaysKvModel>?>(
+              future: DaysAdsService().fetchDays(),
+              builder: (context, AsyncSnapshot<List<DaysKvModel>?> snapshot){
+                if(snapshot.hasData){
+                  return   ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (BuildContext contex, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: SingleChildScrollView(
                             child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(6.0),
-                                      child: Text(
-                                        'Student bollarga kvartira !!!',
-                                        style: TextStyle(fontSize: 18.sp),
+                                Container(
+                                  width: 324.w,
+                                  height: 100.h,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(6.r),
+                                      color: AppColors.secondBackgroud),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(6.0),
+                                            child: Text(
+                                              '${snapshot.data![index].title} !!!',
+                                              style: TextStyle(fontSize: 18.sp),
+                                            ),
+                                          ),
+                                          const Padding(
+                                            padding: EdgeInsets.fromLTRB(1, 0, 8, 0),
+                                            child: Icon(
+                                              Icons.favorite_border,
+                                              color: AppColors.error,
+                                            ),
+                                          )
+                                        ],
                                       ),
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.fromLTRB(1, 0, 8, 0),
-                                      child: Icon(
-                                        Icons.favorite_border,
-                                        color: AppColors.error,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                                  child: Text(
-                                    '300 y.e/oyiga',
-                                    style: TextStyle(
-                                        color: AppColors.mainColor,
-                                        fontSize: 24.sp),
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                                      child: Text(
-                                        "Chilonzor tumani 2 kv 5/34 4 xonadon",
-                                        style: TextStyle(fontSize: 10.sp),
-                                      ),
-                                    ),
-                                    InkWell(
-                                      onTap: (){
-                                        Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailPage()));
-                                      },
-                                      child: const Padding(
-                                        padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                                      Padding(
+                                        padding:
+                                        const EdgeInsets.fromLTRB(8, 0, 8, 0),
                                         child: Text(
-                                          'Batafsil',
+                                          '${snapshot.data![index].cost}',
                                           style: TextStyle(
-                                              decoration:
-                                                  TextDecoration.underline,
-                                              color: AppColors.mainColor),
+                                              color: AppColors.mainColor,
+                                              fontSize: 24.sp),
                                         ),
                                       ),
-                                    )
-                                  ],
+                                      Row(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Padding(
+                                            padding:
+                                            const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                                            child: Text(
+                                              "${snapshot.data![index].address}",
+                                              style: TextStyle(fontSize: 10.sp),
+                                            ),
+                                          ),
+                                          InkWell(
+                                            onTap: (){
+                                              Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailPage(
+
+                                              )));
+                                            },
+                                            child: const Padding(
+                                              padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                                              child: Text(
+                                                'Batafsil',
+                                                style: TextStyle(
+                                                    decoration:
+                                                    TextDecoration.underline,
+                                                    color: AppColors.mainColor),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height: 18.h,
+                                      )
+                                    ],
+                                  ),
                                 ),
-                                SizedBox(
-                                  height: 18.h,
-                                )
                               ],
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  );
-                }),
+                        );
+                      });
+                }
+                return Center(child: CircularProgressIndicator(),);
+              },
+
+
+            ),
             ListView.builder(
                 shrinkWrap: true,
                 itemCount: 20,
