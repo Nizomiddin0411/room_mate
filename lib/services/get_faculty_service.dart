bf8c003f38fa07d750f0d3350e2410eb7df69fb5
nizomiddiniminjonov11 <nizomiddiniminjonov11@gmail.com>
2022-09-13 18:24:15 +0500

    universitet fakultet check is ready

--- lib/services/get_faculty_service.dart
+++ lib/services/get_faculty_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:talaba_uy/core/data/mockdata.dart';
 import '../models/get_faculty_model.dart';
 
 
@@ -8,7 +9,7 @@ class GetFacultyService {
   Future<List<GetFacultyModel>?> fetchFaculty() async {
     try {
       var response = await http.get(Uri.parse(
-        'http://164.68.114.231:8081/roommate/backend/web/api/university/get-faculty?university_id=2',
+        'http://164.68.114.231:8081/roommate/backend/web/api/university/get-faculty?university_id=${MockData.id}',
       ),
           headers: {
             HttpHeaders.authorizationHeader: 'Bearer VVuDO41zOoYY2KvUUHYPOJ9PzKsnnJeD'
@@ -19,7 +20,7 @@ class GetFacultyService {
         List json = jsonDecode(response.body);
         List <GetFacultyModel> data = json.map((e) => GetFacultyModel.fromJson(e)).toList();
         // var data = TotalAdsModel.fromJson(jsonDecode(response.body));
-        print('${data}+++++++++++');
+        // print('${data}+++++++++++');
         return data;
 
       }
